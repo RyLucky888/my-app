b2f6fa1e83e1825d4397d426c877a61691d99fc8
Chea,Riyanal <riyanal.chea@outlook.com>
2020-02-12 15:31:10 +1100

    argparse

---
+++ argtest1.py
@@ -0,0 +1,18 @@
+from jinja2 import *
+import sys
+from openpyxl import *
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o','--Option',help='test')
+    args = parser.parse_args()
+    
+class UpgradeServers:
+    def __init__(self,sourcefile):
+        self.sourcefile = sourcefile
+
+class D
+    
+if __name__=='__main__':
+    main()
